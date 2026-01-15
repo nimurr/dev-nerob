33d0ab1366b984f931ffaa8c1774e14ad3156356
nimurr <nimurnerob404@gmail.com>
2026-01-15 12:48:46 +0600

    `Update Footer component with new styles and content`

--- src/Components/Common/Footer.js
+++ src/Components/Common/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-black text-white py-5 text-xl text-center mt-5'>
-            Footer 
+        <div className='bg-[#13062460] text-sm text-white py-5  text-center border-t-2 border-primary'>
+            <p>Copyright &copy; 2025. All rights reserved. <span>Developed by <span className='font-bold text-primary'>Nimur Rahman Nerob</span></span> </p>
         </div>
     );
 }
--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -25,7 +25,7 @@ export default function Layout({ children }) {
         <div className="bg-tertiary relative bg-grid-pattern">
             <Header />
             {children}
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 }
